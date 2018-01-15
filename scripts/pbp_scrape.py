5744223d2fefaa58a2161c3684a5f3193c95659f
Chirag Jain <csjain@ncsu.edu>
2018-01-15 10:57:51 -0500

    Updated script to get pbp info for all games of a given year

--- scripts/pbp_scrape.py
+++ scripts/pbp_scrape.py
@@ -5,8 +5,30 @@ import ssl
 from urllib import request
 from bs4 import BeautifulSoup
 
+def getGames(year):
+
+    context = ssl._create_unverified_context()
+    gameUrl = "https://www.pro-football-reference.com/years/" + year
+    rawPageData = request.urlopen(gameUrl, context=context).read().decode()
+
+    rawSoup = BeautifulSoup(rawPageData, 'lxml')
+    outerDiveSoup = rawSoup.find(id='all_playoff_results')
+    mainSoup = BeautifulSoup(str(outerDiveSoup).replace('<!--','').replace('-->',''), 'lxml')
+    mainData = mainSoup.find_all('tr')
+
+    games = []
+    for tableRow in mainData:
+        textdata = ''
+        dataRow = tableRow.find_all('td')
+        for dataElement in dataRow:
+            if dataElement.get_text() == "boxscore":
+                games += (dataElement.find("a").get('href').split('/')[2]).split('.')[0],
+
+    return games
+
 def savePlayByPlayData(game):
 
+    print("Getting Play by Play data for game {0}".format(game))
     context = ssl._create_unverified_context()
     gameUrl = "http://www.pro-football-reference.com/boxscores/" + game + ".htm"
     rawPageData = request.urlopen(gameUrl, context=context).read().decode()
@@ -16,21 +38,20 @@ def savePlayByPlayData(game):
     mainSoup = BeautifulSoup(str(outerDiveSoup).replace('<!--','').replace('-->',''), 'lxml')
     mainData = mainSoup.find_all('tr')
 
-    with open('data/pbp/' + game + '.csv', 'w') as outputHandle:
+    with open('../data/pbp/' + game + '.txt', 'w') as outputHandle:
         for tableRow in mainData:
             textdata = ''
-            dataRow = tableRow.find_all('td')
-            if len(dataRow) == 0:
-                dataRow = tableRow.find_all('th')
+            dataRow = tableRow.find_all('th') + tableRow.find_all('td')
             for dataElement in dataRow:
                 textdata += dataElement.get_text() + '|'
             outputHandle.write(textdata[:-1] + '\n')
 
 def main():
-    games = ["201709170nor"]
-
-    for game in games:
-        savePlayByPlayData(game)
+    years =[str(x + 2000) for x in range(18)]
+    for year in years:
+        games = getGames(year)
+        for game in games:
+            savePlayByPlayData(game)
 
 if __name__ == "__main__":
     main()
