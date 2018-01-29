c9b7ee8645c29d3533fbdcf0e0dffe2776936732
Chirag Jain <csjain@ncsu.edu>
2018-01-29 14:35:52 -0500

    Added file caching and pandas dataframe option

--- scripts/pbp_scrape.py
+++ scripts/pbp_scrape.py
@@ -4,6 +4,8 @@ import ssl
 
 from urllib import request
 from bs4 import BeautifulSoup
+from pathlib import Path
+import pandas as pd
 
 def getGames(year):
 
@@ -26,32 +28,61 @@ def getGames(year):
 
     return games
 
-def savePlayByPlayData(game):
+def getPlayByPlayData(game, save=True, matrix=False, dataframe=False):
 
-    print("Getting Play by Play data for game {0}".format(game))
     context = ssl._create_unverified_context()
     gameUrl = "http://www.pro-football-reference.com/boxscores/" + game + ".htm"
-    rawPageData = request.urlopen(gameUrl, context=context).read().decode()
+
+    if Path('../data/rawpage/'+ game + '.html').exists():
+        print("Getting Play by Play data for game {0} from cache".format(game))
+        with open('../data/rawpage/'+ game + '.html', 'r') as inputHandle:
+            rawPageData = inputHandle.read()
+    else:
+        print("Getting Play by Play data for game {0} from web".format(game))
+        rawPageData = request.urlopen(gameUrl, context=context).read().decode()
+        with open('../data/rawpage/'+ game + '.html', 'w') as outputHandle:
+            outputHandle.write(rawPageData)
 
     rawSoup = BeautifulSoup(rawPageData, 'lxml')
     outerDiveSoup = rawSoup.find(id='all_pbp')
     mainSoup = BeautifulSoup(str(outerDiveSoup).replace('<!--','').replace('-->',''), 'lxml')
     mainData = mainSoup.find_all('tr')
 
-    with open('../data/pbp/' + game + '.txt', 'w') as outputHandle:
+    matrixData = []
+
+    with open('../data/pbp/' + game + '.txt', 'w') as fileHandle:
+        headerAdded = False
         for tableRow in mainData:
             textdata = ''
             dataRow = tableRow.find_all('th') + tableRow.find_all('td')
+            if len(dataRow) == 1 or (dataRow[0].get_text() == 'Quarter' and headerAdded):
+                continue
+
             for dataElement in dataRow:
                 textdata += dataElement.get_text() + '|'
-            outputHandle.write(textdata[:-1] + '\n')
+            fileHandle.write(textdata[:-1] + '\n')
+
+            if not headerAdded :
+                headerAdded = True
+
+            if matrix or dataframe:
+                matrixData.append(textdata[:-1].split('|'))
+    if matrix:
+        return matrixData
+    if dataframe:
+        df = pd.DataFrame(matrixData)
+        df.columns = df.iloc[0]
+        df.drop(df.index[0], inplace=True)
+        return df
 
 def main():
     years =[str(x + 1994) for x in range(24)]
     for year in years:
         games = getGames(year)
         for game in games:
-            savePlayByPlayData(game)
+            print(getPlayByPlayData(game, dataframe=True))
+            break
+        break
 
 if __name__ == "__main__":
     main()
